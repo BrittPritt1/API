53dfc9693fc4623b6ccd1dc8c5799b0acf8bc5be
Britt Breugelmans <breugelmans.britt@gmail.com>
2026-03-15 22:13:26 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿public class NumbersFact
+{
+    public static async Task Main()
+    {
+        Console.WriteLine("This application gives you a random fact about a date.");
+        Console.Write("Enter a month: ");
+        int month = int.Parse(Console.ReadLine());
+        Console.Write("Enter a day: ");
+        int day = int.Parse(Console.ReadLine());
+        
+        string url = $"http://numbersapi.com/{month}/{day}/date";
+
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync(url);
+
+        string message = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine("Fact: " + message);
+    }
+}
